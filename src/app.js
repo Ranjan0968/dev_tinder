1d560c39744505add7596d3f2d5d985a944bc771
Ranjan0968 <nayakreactjs@gmail.com>
2025-01-24 22:23:32 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{
-  res.send('Hello from server for Home.......')
+
+
+app.get("/user/:userId/:name",(req,res)=>{
+  console.log(req.params);
+  res.send({FirstName:"Soumyaranjan",LastName:"Nayak"})
 })
 
+app.post("/user",(req,res)=>{
+  res.send("User save Successfully")
+})
+app.delete("/user",(req,res)=>{
+  res.send("User deleted Successfully")
+})
 app.use("/ranjan",(req,res)=>{
   res.send('Hello from server.......')
 })
+
+
 
 app.listen('7777',()=>{
     console.log("Server is running successfully on port 7777")
