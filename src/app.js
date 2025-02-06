268944a79f43ec45b1d2544929cf3397a69429e2
Ranjan0968 <nayakreactjs@gmail.com>
2025-02-06 23:30:05 +0530

    remove env config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectionDB = require('./config/database');
 const User = require("./models/userSchema")
-require('dotenv').config();
+
 
 const app = express();
 
